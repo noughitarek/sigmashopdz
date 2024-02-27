8ea039af9db78ce3657bc77eefef6e39c74ffefe
Tarek <noughitarek@gmail.com>
2024-02-27 09:06:45 +0100

    bug fix

--- app/Console/Commands/OrdersStatesRefreshCommande.php
+++ app/Console/Commands/OrdersStatesRefreshCommande.php
@@ -27,7 +27,7 @@ class OrdersStatesRefreshCommande extends Command
     public function handle()
     {
         #Order::Save_orders();
-        $orders = Order::orderBy('updated_at', 'desc')->get();
+        $orders = Order::orderBy('updated_at', 'asc')->get();
         foreach($orders as $order){
             if($order->State() != "Archived" && 
                 $order->State() != "Doubled" && 
